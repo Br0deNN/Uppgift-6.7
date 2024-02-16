5ffad55d495f21081c52177096b6e8e031e9f671
7R1XY <theo.broden@gmail.com>
2024-02-16 11:15:19 +0100

    Add project files.

---
+++ Uppgift 6.7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ett tal du vill undersöka");
+        int x = int.Parse(Console.ReadLine());
+        Primtal(x);
+        
+        
+    }
+    static int Primtal(int x)
+    {
+        for (int i = 2; i < x; i++)
+        {
+            if (x % i == 0)
+            {
+                Console.WriteLine("Talet är inte ett primtal");
+                return x;
+            }
+            else
+            {
+                Console.WriteLine("Talet är ett primtal");
+                return x;
+            }
+        }
+        return x;
+        
+    }
+}
